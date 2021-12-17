00bada5841e609c0506d4192b8bd4434b0ab9d72
Angel Dimitriev <achi_d@abv.bg>
2021-12-17 11:33:22 -0500

    Update Source.cpp

--- Sem. 10/Source.cpp
+++ Sem. 10/Source.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include "BST.h"
+#include "BST.hpp"
 
 int main()
 {
-	BST bst;
+	BST<int> bst;
 
 	bst.insert(2);
 	bst.insert(1);
 	bst.insert(4);
 	bst.insert(3);
 	bst.insert(5);
+
 
 	bst.print();
 	std::cout << "------------------------------\n";
@@ -25,7 +26,37 @@ int main()
 	bst.print();
 	std::cout << std::endl;
 
-	BST bst2 = bst;
+	std::cout << "------------------------------\n";
+
+	bst.insert(9);
+	std::cout << "After inserting 9:\n\n";
+	bst.print();
+	std::cout << std::endl;
+
+	std::cout << "------------------------------\n";
+
+	bst.insert(0);
+	std::cout << "After inserting 0:\n\n";
+	bst.print();
+	std::cout << std::endl;
+
+
+	std::cout << "------------------------------\n";
+
+	bst.remove(5);
+	std::cout << "After removing 5:\n\n";
+	bst.print();
+	std::cout << std::endl;
+
+	std::cout << "------------------------------\n";
+
+
+	bst.remove(3);
+	std::cout << "After removing 3:\n\n";
+	bst.print();
+	std::cout << std::endl;
+
+	BST<int> bst2 = bst;
 	std::cout << "------------------------------\n";
 	std::cout << "After copy:\n";
 	bst.print();
