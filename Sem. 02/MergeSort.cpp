f61d0fac4062abf035dd169b58f227bbf6b3554c
Angel Dimitriev <achi_d@abv.bg>
2023-03-05 02:04:01 +0200

    Update MergeSort.cpp

--- Sem. 02/MergeSort.cpp
+++ Sem. 02/MergeSort.cpp
@@ -1,59 +1,62 @@
 #include <iostream>
+using namespace std;
 
-template <class T> void mergeSort(T* arr, int len);
-
-template <class T> void merge(T* arr1, size_t len1,T* arr2, size_t len2);
 
 template <class T>
-void mergeSort(T* arr, int len)
-{  
-    if(len<=1)
-        return;
-        
-    size_t mid = len/2;
-
-    mergeSort(arr, mid);
-    mergeSort(arr + mid, len - mid);
-  
-    merge<T>(arr, mid, arr+mid, len-mid);
- 
-}
-template <class T>
-void merge(T* arr1, size_t arr1len,T* arr2, size_t arr2len)
+void Merge(T* firstArr, size_t firstSize, T* secondArr, size_t secondSize, T* resultArr)
 {
-    T* result = new T[arr1len + arr2len];
+	size_t resultIter = 0; 
+	size_t firstIter = 0;
+	size_t secondIter = 0;
 
-    size_t cursor1 = 0, cursor2 = 0, resultCursor = 0;
+	while(firstIter < firstSize && secondIter < secondSize)
+		resultArr[resultIter++] = (firstArr[firstIter] < secondArr[secondIter] ? firstArr[firstIter++] : secondArr[secondIter++]);
 
-    while(cursor1 < arr1len && cursor2 < arr2len)
-    {
-        if(arr1[cursor1] <= arr2[cursor2])
-            result[resultCursor++] = arr1[cursor1++];
-        else  
-            result[resultCursor++] = arr2[cursor2++];
-    }
+	while(firstIter < firstSize)
+		resultArr[resultIter++] = firstArr[firstIter++];
 
-    while(cursor1 < arr1len)
-        result[resultCursor++]=arr1[cursor1++];
-
-    while(cursor2 < arr2len)
-        result[resultCursor++]=arr2[cursor2++];
-        
-   //Въпреки,че arr1 е с по-малка големина, можем да си ползволим това да излезем извън масива, понеже знаем, че arr1 с arr2 са слепени     
-    for(size_t i = 0; i < (arr1len + arr2len); i++)
-        arr1[i] = result[i];
-    delete[] result;
+	while(secondIter < secondSize)
+		resultArr[resultIter++] = secondArr[secondIter++];
 }
 
+template <class T>
+void MergeSortStep(T* pArr, size_t Size, T* pBuffer)
+{
+	if(Size <= 1)
+		return;
+
+	size_t middle = Size / 2;
+
+	MergeSortStep(pArr, middle, pBuffer);
+
+	MergeSortStep(pArr + middle, Size - middle, pBuffer + middle);
+
+	Merge(pArr, middle, pArr + middle, Size - middle, pBuffer);
+
+	for(size_t i = 0; i < Size; i++)
+		pArr[i] = pBuffer[i];
+}
+
+template <class T>
+void MergeSort(T* pArr, size_t Size)
+{
+	if (!pArr || Size == 0)
+		return;
+
+	T* pBuffer = new T[Size];
+
+	MergeSortStep(pArr, Size, pBuffer);
+
+	delete[]  pBuffer;
+}
+
+
+const int SIZE = 15;
 int main()
 {
-    const size_t SIZE = 9;
-    int arr[SIZE] = { 9,8,7,6,5,4,3,2,1 };
+	int arr1[] = { 15,14,13,12,11,30,90,8,7,6,5,4,3,2,1};
+	MergeSort(arr1, SIZE);
 
-    mergeSort<int>(arr, SIZE);
-
-    for (int i = 0; i < SIZE; i++)
-        std::cout << arr[i] << ' ';
-    std::cout << std::endl;
-
+	for (int i = 0; i < SIZE; i++)
+		cout << arr1[i] << " ";
 }
