c13136d728568ffdf553ac750d47272a06b76f6f
Angel Dimitriev <achi_d@abv.bg>
2024-05-28 17:16:29 +0300

    Update Async_fibb.cpp

--- advanced_Multithreading/Async_fibb.cpp
+++ advanced_Multithreading/Async_fibb.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include <vector>
 #include <thread>
 #include <mutex>
 #include <stdint.h>
@@ -11,79 +10,79 @@
 
 using namespace std;
 
-
 namespace Fibb
 {
-	std::queue<int> tasks;
-	std::mutex workProtect;
-	std::condition_variable cv;
+    std::queue<int> tasks;
+    std::mutex workProtect;
+    std::condition_variable cv;
 
-	bool isWorking = false;
+    bool isWorking = false;
 
-	size_t fibb(int n)
-	{
-		if (n == 0 || n == 1)
-			return 1;
-		return fibb(n - 1) + fibb(n - 2);
-	}
+    size_t fibb(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        return fibb(n - 1) + fibb(n - 2);
+    }
 
-	void worker()
-	{
-		while (true)
-		{
-			int current = 0;
-			{
-				if (!isWorking)
-					return;
+    void worker()
+    {
+        while (true)
+        {
+            int current = 0;
+            {
+                std::unique_lock<std::mutex> currentLock(workProtect);
 
-				std::unique_lock<std::mutex> currentLock(workProtect);
+                if (!isWorking && tasks.empty())
+                    return;
 
-				if (tasks.empty())
-				{
-					cv.wait(currentLock, []() {return !isWorking || !tasks.empty(); });
-				}
+                if (tasks.empty())
+                {
+                    cv.wait(currentLock, []() { return !isWorking || !tasks.empty(); });
+                }
 
-				if (tasks.empty())
-					continue;
+                if (!isWorking && tasks.empty())
+                    return;
 
+                current = tasks.front();
+                tasks.pop();
+            }
+            size_t result = fibb(current);
+            std::cout << "fibb(" << current << ") = " << result << std::endl;
+        }
+    }
 
-				current = tasks.front();
-				tasks.pop();
-			}
-			size_t result = fibb(current);
-			std::cout << "fibb(" << current << ")" << result << std::endl;
-		}
-	}
+    void async_fibb()
+    {
+        isWorking = true;
+        std::thread th(worker);
 
-	void async_fibb()
-	{
-		isWorking = true;
-		std::thread th(worker);
-
-		while (true)
-		{
-			int input;
-			cin >> input;
-			if (input >= 0)
-			{
-				std::lock_guard<std::mutex> guard(workProtect);
-				tasks.push(input);
-			}
-			else
-				break;
-			cv.notify_one();
-		}
-		{
-			std::lock_guard<std::mutex> guard(workProtect);
-			isWorking = false;
-		}
-		cv.notify_one();
-		th.join();
-		std::cout << "Done!" << std::endl;
-	}
+        while (true)
+        {
+            int input;
+            cin >> input;
+            if (input >= 0)
+            {
+                {
+                    std::lock_guard<std::mutex> guard(workProtect);
+                    tasks.push(input);
+                }
+                cv.notify_one();
+            }
+            else
+                break;
+        }
+        {
+            std::lock_guard<std::mutex> guard(workProtect);
+            isWorking = false;
+        }
+        cv.notify_all();
+        th.join();
+        std::cout << "Done!" << std::endl;
+    }
 }
 
 int main()
 {
-	Fibb::async_fibb();
+    Fibb::async_fibb();
 }
