4ad7f0fdd5be558d2a32134764197750f49e3aa9
Angel Dimitriev <achi_d@abv.bg>
2024-10-08 15:15:12 +0300

    Create linear_search_rec.cpp

---
+++ Searching/linear_search_rec.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+bool linearSearch(const T* arr, unsigned len, const T& searched)
+{
+	if (len == 0)
+		return false;
+	
+	return  *arr == searched || linearSearch(++arr, --len, searched);
+}
+
+template <typename Iterator, typename T>
+Iterator recursive_linear_search(Iterator begin, Iterator end, const T& value)
+{
+    if (begin == end)
+        return end;
+    
+    if (*begin == value) 
+        return begin;
+    
+    return recursive_linear_search(++begin, end, value);
+}
+
+int main()
+{
+	int arr[5] = { 1, 2, 3, 4, 5 };
+	cout << linearSearch<int>(arr, 5, 9);
+}
