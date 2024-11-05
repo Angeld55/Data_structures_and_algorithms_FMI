fb05bf960fe0a2f7a5f4c49d2964ea774c4bd084
Angel Dimitriev <achi_d@abv.bg>
2024-11-05 23:32:01 +0200

    Create inglyLinkedList_mergeSort.cpp

---
+++ Miscellaneous/inglyLinkedList_mergeSort.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data = 0;
+    Node* next = nullptr;
+    
+    Node(int data, Node* next = nullptr) : data(data), next(next){}
+};
+
+void print(const Node* root)
+{
+    while(root)
+    {
+        std::cout << root->data << " ";
+        root = root->next;
+    }
+    std::cout << std::endl;
+}
+
+void freeList(Node* root)
+{
+    Node* current = root;
+    while(current)
+    {
+        Node* next = current->next;
+        delete current;
+        current = next;
+    }
+}
+
+void pushBack(Node*& resultBegin, Node*& resultEnd, Node* toAdd)
+{
+	if (!resultBegin)
+	{
+		resultBegin = resultEnd = toAdd;
+	}
+	else
+	{
+		resultEnd->next = toAdd;
+		resultEnd = toAdd;
+	}
+}
+
+Node* mergeLists(Node* first, Node* second)
+{
+	Node* resultBegin = nullptr, *resultEnd = nullptr;
+
+	while (first && second)
+	{
+		if (first->data < second->data)
+		{
+			pushBack(resultBegin, resultEnd, first);
+			first = first->next;
+		}
+		else
+		{
+			pushBack(resultBegin, resultEnd, second);
+			second = second->next;
+		}
+	}
+	if (!first)
+	{
+		pushBack(resultBegin, resultEnd, second);
+	}
+	else
+	{
+		pushBack(resultBegin, resultEnd, first);
+	}
+	return resultBegin;
+}
+
+
+Node* getMid(Node* list)
+{
+    Node* slow = list;
+    Node* fast = list ->next;
+    
+    while(fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+Node* mergeSort(Node* list)
+{
+    
+    if(!list || !list->next) 
+        return list;
+        
+    Node* mid = getMid(list);
+
+        
+    Node* left = list;
+    Node* right = mid->next;
+    mid->next = nullptr;
+    
+    left = mergeSort(left);
+    right = mergeSort(right);
+    
+    return mergeLists(left, right);
+}
+
+
+int main()
+{
+    Node* list = new Node(20, new Node(14, new Node(7)));
+    list = mergeSort(list);
+    print(list);
+    freeList(list);
+    list = nullptr;
+}
