d9bf8e56dec7bdeedf00c3d7137f7433141c52ce
Elena Tuparova <elena.tuparova@gmail.com>
2024-11-14 20:20:53 +0200

    Create checkValidBracketsString.cpp

---
+++ Miscellaneous/checkValidBracketsString.cpp
@@ -0,0 +1,52 @@
+#include <stack>
+#include <string>
+#include <iostream>
+
+bool isOpeningBracket(char ch)
+{
+	return ch == '(' || ch == '[' || ch == '{';
+}
+
+bool isMatchingBracket(char ch1, char ch2)
+{
+	switch (ch1)
+	{
+	case '(':
+		return ch2 == ')';
+	case '[':
+		return ch2 == ']';
+	case '{':
+		return ch2 == '}';
+	}
+
+	return false;
+}
+
+bool isCorrect(const std::string& str)
+{
+	std::stack<char> brackets;
+
+	for (char ch : str)
+	{
+		if (isOpeningBracket(ch))
+		{
+			brackets.push(ch);
+		}
+		else {
+			if (brackets.empty() || !isMatchingBracket(brackets.top(), ch))
+			{
+				return false;
+			}
+			else
+			{
+				brackets.pop();
+			}
+		}
+	}
+	return brackets.empty();
+}
+
+int main()
+{
+	std::cout << isCorrect("()");
+}
