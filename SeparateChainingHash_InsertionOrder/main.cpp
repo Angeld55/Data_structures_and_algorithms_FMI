eb30857b0da345a3aabcc3ea4b1131fe324c8dd4
Angel Dimitriev <achi_d@abv.bg>
2024-12-20 16:28:23 +0200

    Create main.cpp

---
+++ SeparateChainingHash_InsertionOrder/main.cpp
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <iostream>
+
+int main() 
+{
+    InsertionOrderHashMap<std::string, int> map;
+
+    map.add("apple", 10);
+    map.add("banana", 20);
+    map.add("cherry", 30);
+
+    std::cout << "Contents of the map:\n";
+    for (auto it = map.cbegin(); it != map.cend(); ++it) {
+        std::cout << (*it).first << ": " << (*it).second << "\n";
+    }
+
+    std::cout << "\nFinding 'banana':\n";
+    auto found = map.find("banana");
+    if (found != map.cend()) {
+        std::cout << (*found).first << ": " << (*found).second << "\n";
+    }
+    else {
+        std::cout << "Key not found.\n";
+    }
+
+    std::cout << "\nRemoving 'apple'...\n";
+    map.remove("apple");
+
+    std::cout << "Contents of the map after removal:\n";
+    for (auto it = map.cbegin(); it != map.cend(); ++it) 
+        std::cout << (*it).first << ": " << (*it).second << "\n";
+    
+
+    return 0;
+}
