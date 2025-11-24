28ee05e170ee438e1d6b3b8f6d3e77a4c49f431b
Angel Dimitriev <achi_d@abv.bg>
2025-11-24 10:38:11 +0200

    Update singlyLinkedList_quickSort.cpp

--- Miscellaneous/singlyLinkedList_quickSort.cpp
+++ Miscellaneous/singlyLinkedList_quickSort.cpp
@@ -1,116 +1,138 @@
 #include <iostream>
 
-struct Node
-{
-	int data;
-	Node* next = nullptr;
-	Node(int value, Node* ptr = nullptr) : data(value), next(ptr) {}
+struct Node {
+    int data;
+    Node* next = nullptr;
+
+    Node(int value, Node* ptr = nullptr)
+        : data(value), next(ptr) {}
+};
+
+struct PartitionResult {
+    Node* lessHead;          // елементи, които удовлетворяват предиката
+    Node* greaterEqualHead;  // останалите елементи
+};
+
+struct ListSegment {
+    Node* head;
+    Node* tail;
 };
 
 void push_back(Node*& begin, Node*& end, Node* toAdd)
 {
-	if (begin == nullptr)
-	{
-		begin = end = toAdd;
-	}
-	else
-	{
-		end->next = toAdd;
-		end = toAdd;
-		///toAdd->next = nullptr !!! NO! You need this pointer in the next iteration of the partition func
-	}
+    if (begin == nullptr)
+    {
+        begin = end = toAdd;
+    }
+    else
+    {
+        end->next = toAdd;
+        end = toAdd;
+        ///toAdd->next = nullptr !!! NO! You need this pointer in the next iteration of the partition func
+    }
 }
 
 template <class PredicateType>
-std::pair<Node*, Node*> partition(Node* list, const PredicateType& pred)
+PartitionResult partition(Node* list, const PredicateType& pred)
 {
-	Node* trueListBegin = nullptr;
-	Node* trueListEnd = nullptr;
+    Node* trueListBegin = nullptr;
+    Node* trueListEnd   = nullptr;
 
-	Node* falseListBegin = nullptr;
-	Node* falseListEnd = nullptr;
+    Node* falseListBegin = nullptr;
+    Node* falseListEnd   = nullptr;
 
-	while (list != nullptr)
-	{
-		if (pred(list->data))
-			push_back(trueListBegin, trueListEnd, list);
-		else
-			push_back(falseListBegin, falseListEnd, list);
-		list = list->next;
-	}
-	if (trueListEnd)
-	{
-		trueListEnd->next = nullptr;
-	}
-	if (falseListEnd)
-	{
-		falseListEnd->next = nullptr;
-	}
+    while (list != nullptr)
+    {
+        if (pred(list->data))
+            push_back(trueListBegin, trueListEnd, list);
+        else
+            push_back(falseListBegin, falseListEnd, list);
 
-	return std::make_pair(trueListBegin, falseListBegin);
+        list = list->next;
+    }
+
+    if (trueListEnd)
+        trueListEnd->next = nullptr;
+
+    if (falseListEnd)
+        falseListEnd->next = nullptr;
+
+    return { trueListBegin, falseListBegin };
 }
 
 // <begin, end>
-std::pair<Node*, Node*> concatLists(Node* leftBegin, Node* leftEnd, Node* rightBegin, Node* rightEnd) 
+std::pair<Node*, Node*> concatLists(Node* leftBegin, Node* leftEnd,
+                                    Node* rightBegin, Node* rightEnd)
 {
-	if (leftBegin == nullptr) return std::make_pair(rightBegin, rightEnd); 
-	if (rightBegin == nullptr) return std::make_pair(leftBegin, leftEnd);
+    if (leftBegin == nullptr)
+        return std::make_pair(rightBegin, rightEnd);
 
-	leftEnd->next = rightBegin;
-	return std::make_pair(leftBegin, rightEnd);
+    if (rightBegin == nullptr)
+        return std::make_pair(leftBegin, leftEnd);
+
+    leftEnd->next = rightBegin;
+    return std::make_pair(leftBegin, rightEnd);
 }
 
-
-std::pair<Node*, Node*> quickSort(Node* list)
+ListSegment quickSort(Node* list)
 {
-	if (!list || !list->next)
-		return std::make_pair(list, list);
+    if (!list || !list->next)
+        return { list, list };
 
-	int pivot = list->data;
-	auto partitionResults = partition(list, [pivot](int el) {return el < pivot; });
+    int pivot = list->data;
 
-	Node* pivotPtr = partitionResults.second;
+    PartitionResult parts =
+        partition(list, [pivot](int el) { return el < pivot; });
 
-	auto leftToConcat = quickSort(partitionResults.first);
-	auto rightToConcat = quickSort(partitionResults.second->next); //skip the pivot
+    Node* pivotPtr = parts.greaterEqualHead;  // We choose the first el as pivot
 
-	pivotPtr->next = rightToConcat.first; //attach the pivot to the begin of right
-	rightToConcat.first = pivotPtr;
-	if (!rightToConcat.second) //the right was empty list, we should concat only the pivot
-		rightToConcat.second = pivotPtr;
+    // sort all elements < pivot
+    ListSegment left  = quickSort(parts.lessHead);
 
-	return concatLists(leftToConcat.first, 
-					   leftToConcat.second,
-		               rightToConcat.first,
-		               rightToConcat.second);
+    // sort all elements >= pivot
+    ListSegment right = quickSort(pivotPtr->next); // we skip the pivot!!
+
+    // push_front the pivot infront of the right segment
+    pivotPtr->next = right.head;
+    right.head = pivotPtr;
+    if (!right.tail)
+        right.tail = pivotPtr;
+
+    auto merged = concatLists(left.head, left.tail,
+                              right.head, right.tail);
+
+    return { merged.first, merged.second };
 }
 
 void printList(Node* iter)
 {
-	while (iter != nullptr)
-	{
-		std::cout << iter->data;
-		if (iter->next)
-			std::cout << " -> ";
-		iter = iter->next;
-	}
+    while (iter != nullptr)
+    {
+        std::cout << iter->data;
+        if (iter->next)
+            std::cout << " -> ";
+        iter = iter->next;
+    }
 }
+
 void freeList(Node* iter)
 {
-	while (iter != nullptr)
-	{
-		Node* toDelete = iter;
-		iter = iter->next;
-		delete toDelete;
-	}
+    while (iter != nullptr)
+    {
+        Node* toDelete = iter;
+        iter = iter->next;
+        delete toDelete;
+    }
 }
 
 int main()
 {
-	Node* list = new Node{ 31, new Node{4, new Node{53, new Node{2}}} };
-	
-	auto res = quickSort(list);
-	list = res.first;
-	printList(list);
-	freeList(list);
+    Node* list = new Node{ 31,
+                    new Node{ 4,
+                    new Node{ 53,
+                    new Node{ 2 }}}};
+
+    ListSegment sorted = quickSort(list);
+    printList(sorted.head);
+    freeList(sorted.head);
 }
